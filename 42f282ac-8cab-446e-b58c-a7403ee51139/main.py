7189e67c2aac384485b4a7108f521529991a789d
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-09-26 19:14:08 +0500

    User Haris Javed saved changes to main.py at 09/26/2024, 14:14:07 UTC time.

--- 42f282ac-8cab-446e-b58c-a7403ee51139/main.py
+++ 42f282ac-8cab-446e-b58c-a7403ee51139/main.py
@@ -1,47 +1,28 @@
-from surmount.base_class import Strategy, TargetAllocation
-from surmount.technical_indicators import RSI, SMA
-from surmount.logging import log
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
 
 class TradingStrategy(Strategy):
-    def __init__(self):
-        # Tickers of interest
-        self.tickers = ["AAPL"]
-        self.sma14 = 14 # Length for SMA calculation
-        self.rsi_length = 14  # Length for RSI calculation
-        self.oversold_threshold = 30  # RSI threshold for oversold condition.overbought_threshold = 70  # RSI threshold for overbought condition
 
     @property
     def assets(self):
-        return self.tickers
+        return ["SPY"]
 
     @property
     def interval(self):
-        # Define the interval for the strategy. Adjust as per strategy requirement.
-        return "1day"
+        return "1hour"
 
-    def run(self, data):
-        # Initialize the allocation at 0 for the strategy start. Adjust according to the strategy logic.
-        allocation = 0.0
+    def run(self, data_functions):
+        data = data_functions["ohlcv"]
 
-        # Get the latest data for calculation
-        ohlcv_data = data["ohlcv"]
-        current_price = ohlcv_data["-L"]["close"]
-        sma_values = SMA("AAPL", ohlcv_data, self.sma_length)
-        rsi_values = RSI("AAPL", ohlcv_data, self.rsi_length)
+        spy_20_ma = SMA("SPY", data, 20)
+        spy_10_ma = SMA("SPY", data, 10)
+        spy_10_rsi = RSI("SPY", data, 10)
 
-        # Make sure we have enough data points for both SMA and RSI
-        if sma_values is not None and rsi_values is not None and len(sma_values) > 0 and len(rsi_values) > 0:
-            latest_sma = sma_values[-1]
-            latest_rsi = rsi_values[-1]
+        if None in [spy_20_ma, spy_10_ma, spy_10_rsi]:
+            return None
 
-            # Strategy logic: Enter or increase position if RSI is below oversold_threshold and price is above SMA
-            if latest_rsi < self.oversold_threshold and current_price > latest_sma:
-                allocation = 1.0  #
-            # Exit or decreaseSI is above overbought_threshold
-            elif latest_rsi > self_threshold:
-                allocation = 0.0  # No allocation
+        spy_stake = 0
+        if spy_10_rsi[-1]<60 and spy_10_ma[-1]>spy_20_ma[-1]:
+            spy_stake = 1
 
-        # Log for monitoring
-        log(f"AAPL Strategy Allocation: {allocation}")
-        # Return the allocation decision
-        return TargetAllocation({"AAPL": allocation})
+        return TargetAllocation({"SPY": spy_stake})
